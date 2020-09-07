a9f4e7f3343a1af35599cdea5bb990fef8dd3f7b
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-09-07 22:58:21 +0100

    Crude breakpointer [needs rewritten, pof]

--- source/application/dashboard/main.lua
+++ source/application/dashboard/main.lua
@@ -1,1 +1,6 @@
 print("Dashboard Entry")
+-- Essential Debug Utilities
+require("devgit:source/application/utilities/debug/output.lua")
+require("devgit:source/application/utilities/debug/keybinds.lua")
+
+local breakpointer = require("devgit:source/libraries/UI-breakpointer/main.lua")
