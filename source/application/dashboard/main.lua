66ad745a086e6939e6fe5174748c3ba33e2db803
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-09-08 00:25:47 +0100

    placeholder dash (poor)

--- source/application/dashboard/main.lua
+++ source/application/dashboard/main.lua
@@ -3,4 +3,15 @@ print("Dashboard Entry")
 require("devgit:source/application/utilities/debug/output.lua")
 require("devgit:source/application/utilities/debug/keybinds.lua")
 
-local breakpointer = require("devgit:source/libraries/UI-breakpointer/main.lua")
+local colours = require("devgit:source/application/utilities/colourScheme.lua")
+
+local main = core.construct("guiFrame", {
+    parent = teverse.interface,
+    size = guiCoord(1, 0, 1, 0)
+})
+
+local sidebar = require("devgit:source/application/dashboard/sidebar.lua")
+sidebar.addButton("Home", "th", require("devgit:source/application/dashboard/pages/home.lua"))
+sidebar.addButton("Apps", "book")
+sidebar.addButton("Develop", "code")
+sidebar.addButton("Tutorials", "graduation-cap")
