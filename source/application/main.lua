87295905b3904c2b6bc77cf9cd1f06bf14f1a4e6
u-train <skassem05@outlook.com>
2020-09-07 20:10:30 -0400

    mfw when u forgot debug code.

--- source/application/main.lua
+++ source/application/main.lua
@@ -8,26 +8,4 @@ require("tevgit:source/application/utilities/debug/output.lua")
 require("tevgit:source/application/utilities/debug/keybinds.lua")
 
 -- List of stuff that updates
---require("tevgit:source/application/updater/main.lua")
-
-local class = require("tevgit:source/libraries/UI/constraints/main.lua").newController
-local i = 1
-local a = class("gridLayout")
-a.container.parent = core.interface
-a.container.size = guiCoord(1, 0, 1, 0)
-local box = core.construct("guiFrame")
-box.backgroundColour = colour.random()
-box.parent = a.container
-local box = core.construct("guiFrame")
-box.backgroundColour = colour.random()
-box.parent = a.container
-local box = core.construct("guiFrame")
-box.backgroundColour = colour.random()
-box.parent = a.container
-local box = core.construct("guiFrame")
-box.backgroundColour = colour.random()
-box.parent = a.container
-local box = core.construct("guiFrame")
-box.backgroundColour = colour.random()
-box.parent = a.container
-core.input:on("screenResized", a.refresh)
+require("tevgit:source/application/updater/main.lua")
