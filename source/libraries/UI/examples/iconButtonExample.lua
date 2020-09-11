f8cafad71df2d324bdf9c9417a74047ab7854fff
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-09-11 12:41:11 -0400

    iconButton example w/ new changes

--- source/libraries/UI/examples/iconButtonExample.lua
+++ source/libraries/UI/examples/iconButtonExample.lua
@@ -1,24 +1,17 @@
 local iconButton = require("devgit:source/libraries/UI/components/iconButton.lua")
 
--- Example Button Constructor
-local infoButton = iconButton {
-    parent = nil, -- Set parent
-    position = guiCoord(0.5, -60, 0.5, -20),
-    size = guiCoord(0, 48, 0, 48)
-}
+return function(parent)
+    -- Example Button Constructor
+    local infoButton = iconButton {
+        parent = parent,
+        position = guiCoord(0.5, -60, 0.5, -20),
+        size = guiCoord(0, 48, 0, 48)
+    }
 
--- Set the text of the textButton to 'Home'
---infoButton.setIcon()
-
--- Set the initial state to 'active'
-infoButton.render("active")
-
--- Bind render to state subscriber
-infoButton.states.subscribe(infoButton.render)
-
--- When state is 'focused' invoke action
-infoButton.states.subscribe(function(newState)
-    if newState == "focused" then
-        print("Hello world!")
-    end
-end)
+    -- When the button is pressed, say hello!
+    infoButton.states.subscribe(function(newState)
+        if newState.active then
+            print("Hello world!")
+        end
+    end)
+end
