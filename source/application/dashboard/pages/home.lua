d3d71d3ad20dce4462500bd702681cf338f3896b
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-09-11 12:41:31 -0400

    iconButton usage

--- source/application/dashboard/pages/home.lua
+++ source/application/dashboard/pages/home.lua
@@ -3,14 +3,14 @@
 
 -- Home (default) page-view
 
-local Button = require("devgit:source/libraries/UI/components/textButton.lua")
+local iconButton = require("devgit:source/libraries/UI/components/iconButton.lua")
 
 return function(parent)
     -- Example Button Constructor
-    local infoButton = Button {
+    local infoButton = iconButton {
         parent = parent,
         position = guiCoord(0.5, -60, 0.5, -20),
-        size = guiCoord(0, 178, 0, 48),
+        size = guiCoord(0, 48, 0, 48)
     }
 
     -- When the button is pressed, say hello!
