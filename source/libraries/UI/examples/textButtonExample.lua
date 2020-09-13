c199b325f7bd37745497a3672dffac50ea4d1900
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-09-13 16:20:13 -0400

    Update button-based examples with new changes

---
+++ source/libraries/UI/examples/textButtonExample.lua
@@ -0,0 +1,16 @@
+-- Copyright 2020 - Deviap (deviap.com)
+-- Author(s): Sanjay-B(Sanjay)
+
+-- Text Button Example
+
+local button = require("devgit:source/libraries/UI/components/primaryButton.lua")
+
+return function(parent)
+
+    -- Icon isn't defined in the constructor therefore, it'll be a text button w/o an icon.
+	local _button = button {
+		parent = parent,
+        position = guiCoord(0.5, -0, 0.5, -0),
+        text = "Primary button"
+	}.render()
+end
