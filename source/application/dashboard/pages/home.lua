f9dd45fbd39d695f052fd35048b2a022e961a6ae
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-09-13 16:11:53 -0400

    Simplify home.lua button declaration

--- source/application/dashboard/pages/home.lua
+++ source/application/dashboard/pages/home.lua
@@ -4,42 +4,13 @@
 -- Home (default) page-view
 
 local button = require("devgit:source/libraries/UI/components/primaryButton.lua")
---[[props =
-{
-	parent
-	containerBackgroundColour
-	containerBackgroundAlpha
-	secondaryColour
-	borderWidth
-	borderAlpha
-	borderInset
-}
-]]
+
 return function(parent)
 
-	local b = button {
+	local _button = button {
 		parent = parent,
 		position = guiCoord(0.5, -0, 0.5, -0),
 		iconId = "plus",
 		text = "Primary button"
-	}
-
-	--b.container.position = guiCoord(0.5, -0, 0.5, -0)
-
-	b.render()
-
-	--[[
-    -- Example Button Constructor
-    local infoButton = checkBox {
-        parent = parent,
-        position = guiCoord(0.5, -60, 0.5, -20),
-        size = guiCoord(0, 120, 0, 18)
-    }
-
-    -- When the button is pressed, say hello!
-    --[[infoButton.states.subscribe(function(newState)
-        if newState.active then
-            print("Hello world!")
-        end
-    end)]]--
+	}.render()
 end
