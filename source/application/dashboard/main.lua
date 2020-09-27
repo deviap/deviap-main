8e94967e95e7b341188153c2bfd508b4980e672c
utrain <skassem05@outlook.com>
2020-09-27 14:18:18 -0400

    init for suite

--- source/application/dashboard/main.lua
+++ source/application/dashboard/main.lua
@@ -1,5 +1,8 @@
 print("Dashboard Entry")
 
+core.apps.promptAppDirectory()
+
+--[[
 local main = core.construct("guiFrame", {
 	parent = core.interface,
 	size = guiCoord(1, 0, 1, 0)
@@ -9,4 +12,4 @@ local nav = require("devgit:source/application/dashboard/nav.lua")
 nav.register("devgit:source/application/dashboard/pages/dashboard.lua")
 nav.register("devgit:source/application/dashboard/pages/appLibrary.lua")
 nav.register("devgit:source/application/dashboard/pages/developDashboard.lua")
--- nav.register("devgit:source/application/dashboard/pages/components.lua")
+-- nav.register("devgit:source/application/dashboard/pages/components.lua")]]
---
+++ source/suite/client/scripts/main.lua
@@ -0,0 +1,6 @@
+core.construct("guiTextBox", {
+	parent = core.interface,
+	text = "init",
+	size = guiCoord(1, 0, 1, 0),
+	textAlign = "middle"
+})
