46c30861b95ccd8356aed1ce406efd505b11cd87
utrain <skassem05@outlook.com>
2020-10-02 19:06:06 -0400

    Polish up scripts

--- source/suite/client/scripts/main.lua
+++ source/suite/client/scripts/main.lua
@@ -1,1 +1,8 @@
-core.apps:loadRemote()
+core.construct("guiImage", {
+	image = "devgit:assets/images/Bliss.jpg",
+	size = guiCoord(1, 0, 1, 0),
+	parent = core.interface,
+	zIndex = -1
+})
+
+local a = require("libraries/homeBar.lua")
