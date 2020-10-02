cd5515d31f37086c0bc20260c65c5d772fc6be10
utrain <skassem05@outlook.com>
2020-10-02 11:10:45 -0400

    Fixed entry point for UI-library

--- source/libraries/UI/components.lua
+++ source/libraries/UI/components.lua
@@ -1,9 +1,35 @@
 -- Copyright 2020 - Deviap (deviap.com)
+local dir = "devgit:source/libraries/UI"
 
-local components =
-{
-	button = require("devgit:source/libraries/UI/components/button.lua"),
-	baseComponent = require("devgit:source/libraries/UI/components/baseComponent.lua"),
+local components = {
+	baseComponent = require("./baseComponent.lua")
+	
+	baseButton = require("./components/buttons/baseButton.lua"),
+	primaryButton = require("./components/buttons/primaryButton.lua"),
+	dangerButton = require("./components/buttons/dangerButton.lua"),
+	secondaryButton = require("./components/buttons/secondaryButton.lua"),
+	tertiaryButton = require("./components/buttons/tertiaryButton.lua"),
+	baseComponent = require("./components/baseComponent.lua"),
+
+	appCard = require("./cards/app.lua"),
+
+	dropDown = require("./dropDowns/dropDown.lua"),
+	dropDownOptions = require("./dropDowns/dropDownOptions.lua"),
+
+	textInput = require("./inputs/textInput.lua")
+	numberInput = require("./inputs/numberInput.lua"),
+
+	inlineNotification = require("./notifications/inlineNotification.lua"),
+	multilineNotification = require("./notifications/multilineNotification.lua"),
+
+	tab = require("./tabs/tab.lua"),
+	tabs = require("./tabs/tabs.lua"),
+
+	checkbox = require("./checkbox.lua"),
+	loading = require("./loading.lua"),
+	modal = require("./modal.lua"),
+	toggle = require("./toggle.lua"),
+	tooltip = require("./tooltip.lua"),
 }
 
 return function(name, props)
@@ -16,6 +42,5 @@ return function(name, props)
 		@returns
 			table, component
 	]]
-	return components[name](props)
+	return components[name:gsub("^%.", dir)](props)
 end
-	
