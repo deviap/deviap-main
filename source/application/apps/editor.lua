0e867dcc59125dda36b6d266d036a5cab86997bd
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-11-21 12:51:26 +0000

    local test button

--- source/application/apps/editor.lua
+++ source/application/apps/editor.lua
@@ -29,7 +29,7 @@ local test = core.construct("guiTextBox", {
 	size = guiCoord(0, 90, 0, 16),
 	position = guiCoord(1.0, -140, 1, -16),
 	strokeRadius = 2,
-	text = "Remote Test",
+	text = "Local Test",
 	textAlign = "middle",
 	textSize = 12
 })
@@ -121,74 +121,7 @@ end)
 
 test:on("mouseLeftUp", function()
 	test.visible = false
-	reload.visible = false
+	--reload.visible = false
 
-	local backdrop = core.construct("guiFrame", {
-		parent = core.engine.coreInterface,
-		size = guiCoord(1, 0, 1, 0),
-		position = guiCoord(0, 0, 0, 0),
-		backgroundColour = colour(.25, .25, .25),
-		backgroundAlpha = 0.6,
-		zIndex = 1000
-	})
-
-	local spinner = core.construct("guiIcon", {
-		parent = backdrop,
-		size = guiCoord(0, 40, 0, 40),
-		position = guiCoord(0.5, -20, 0.5, -20),
-		iconMax = 40,
-		iconColour = colour(1, 1, 1),
-		iconType = "faSolid",
-		iconId = "spinner",
-		iconAlpha = 0.9
-	})
-
-	spawn(function()
-		while sleep() and spinner.alive do
-			spinner.rotation = spinner.rotation + math.rad(1)
-		end
-	end)
-
-	core.apps:test()
-	local success, result = core.apps:waitFor("upload")
-	spinner:destroy()
-
-	local container = core.construct("guiTextBox", {
-		parent = backdrop,
-		size = guiCoord(0, 460, 0, 120),
-		position = guiCoord(0.5, -230, 0.5, -60),
-		backgroundColour = colour(1, 1, 1),
-		dropShadowAlpha = 0.3
-	})
-
-	local label = core.construct("guiTextBox", {
-		parent = container,
-		size = guiCoord(1.0, -20, 1.0, -20),
-		position = guiCoord(0, 10, 0, 10),
-		backgroundAlpha = 0.0,
-		textSize = 16,
-		textWrap = true,
-		textAlign = "topLeft"
-		-- textFont = "tevurl:fonts/firaCodeRegular.otf"
-	})
-
-	local title = core.construct("guiTextBox", {
-		parent = backdrop,
-		size = guiCoord(0, 460, 0, 20),
-		position = guiCoord(0.5, -230, 0.5, -80),
-		backgroundColour = colour(0.9, 0.9, 0.9),
-		textSize = 18,
-		dropShadowAlpha = 0.3,
-		textAlign = "middle",
-		zIndex = 2
-	})
-
-	if success then
-		core.apps:reload()
-	else
-		title.backgroundColour = colour.rgb(179, 70, 70)
-		title.textColour = colour.white()
-		title.text = "Something went wrong"
-		label.text = result
-	end
+	core.apps:localTest()
 end)
