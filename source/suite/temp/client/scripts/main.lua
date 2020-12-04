e1a29cf71d130399ca7d3d1f2ac208a8f10c688b
utrain <skassem05@outlook.com>
2020-12-04 13:07:11 -0500

    Added selector

--- source/suite/temp/client/scripts/main.lua
+++ source/suite/temp/client/scripts/main.lua
@@ -1,12 +1,2 @@
-local libs = require("scripts/libraries.lua")
 
-local ui = libs("uiLibrary")
-
-local topBar = ui.newComponent("tabs", {
-	parent = core.interface,
-	size = guiCoord(1, 0, 1, 0)
-})
-
-topBar.addTab(ui.newComponent("tab", {}), core.construct("guiTextBox", { text = "hello world"}))
-
-topBar.render()
+require("scripts/windowTest.lua")
