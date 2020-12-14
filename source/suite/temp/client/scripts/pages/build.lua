1f389444ddf806e19460d25ef90fa3686db7ba3d
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-12-14 17:38:13 -0500

    rebase

--- source/suite/temp/client/scripts/pages/build.lua
+++ source/suite/temp/client/scripts/pages/build.lua
@@ -5,17 +5,19 @@
 -- https://github.com/deviap/deviap-main/blob/master/LICENSE --
 ---------------------------------------------------------------
 -- Author(s): Sanjay-B(Sanjay)
--- Creates the Scene Edit toolbar.
+-- Creates the Build toolbar.
 local gridLayout = require("devgit:source/libraries/UI/constraints/controllers/gridLayout.lua")
 local navItem = require("devgit:source/libraries/UI/components/navigation/navItem.lua")
+local newInstance = require("devgit:source/suite/temp/client/scripts/toolchain/newInstance.lua")
+
 
 return {
-	default = true,
+	default = false,
 	construct = function(parent)
 
 		local layout = gridLayout()
 		layout.container.parent = parent
-		layout.container.position = guiCoord(0, 8, 0, 5)
+		layout.container.position = guiCoord(0, 6, 0, 5)
 		layout.container.size = guiCoord(1, 0, 1, 0)
 		layout.container.canvasSize = guiCoord(1, 0, 1, 0)
 		layout.container.backgroundColour = colour.hex("#FF0000")
@@ -29,34 +31,24 @@ return {
 		layout.fitY = false
 		layout.wrap = true
 
-        local moveTool = navItem {
+        -- Creates a base block instance.
+        local newBlock = navItem {
             parent = layout.container,
             size = guiCoord(0, 28, 0, 28),
             navOrientation = "horizontal",
             iconColour = colour.hex("#212121"),
             iconMax = 20,
-            iconId = "open_with",
-            redirect = nil
-        }
-
-        local rotateTool = navItem {
-            parent = layout.container,
-            size = guiCoord(0, 28, 0, 28),
-            navOrientation = "horizontal",
-            iconColour = colour.hex("#212121"),
-            iconMax = 20,
-            iconId = "cached",
-            redirect = nil
-        }
-
-        local sizeTool = navItem {
-            parent = layout.container,
-            size = guiCoord(0, 28, 0, 28),
-            navOrientation = "horizontal",
-            iconColour = colour.hex("#212121"),
-            iconMax = 20,
-            iconId = "compare_arrows",
-            redirect = nil
+            iconId = "stop",
+            redirect = 
         }
 	end
 }
+
+--[[
+    newInstance("block", {
+        parent = core.scene,
+        position = vector3(0, 10, 0),
+        scale = vector3(5, 5, 5),
+        colour = colour.hex("#616161")
+    })
+]]--
