94273a2fb375b9c7a022a8e7464ac75905b2a43b
Jay <jay@jmbp.local>
2020-12-14 23:56:00 +0000

    select tool use select controller

--- source/suite/client/scripts/pages/sceneEdit/select.lua
+++ source/suite/client/scripts/pages/sceneEdit/select.lua
@@ -5,6 +5,7 @@
 -- https://github.com/deviap/deviap-main/blob/master/LICENSE --
 ---------------------------------------------------------------
 local camera = core.scene.camera
+local selection = require("client/scripts/controllers/selection.lua")
 
 return {
 	name = "select",
@@ -12,7 +13,7 @@ return {
 
 	activate = function(self)
 		-- A cylinder which follows the user's cursor:
-		self.selectionHighlighter = core.construct("block", {
+		self.cursorHighlighter = core.construct("block", {
 			renderQueue = 200, -- this render the block over the rest of the scene
 			emissiveColour = colour.rgb(0, 255, 0),
 			scale = vector3(0.5, 0.01, 0.5),
@@ -20,8 +21,15 @@ return {
 			mesh = "deviap:3d/torus.glb"
 		})
 
-		spawn(function()
+		self.clickEvent = core.input:on("mouseLeftUp", function()
+			if self.hover then
+				selection.set {self.hover}
+			else
+				selection.clear()
+			end
+		end)
 
+		spawn(function()
 			while sleep() and self.active do
 
 				-- Calculate the camera's position and the cursor's direction
@@ -30,15 +38,14 @@ return {
 
 				-- Perform the raycast, exclude our selection highlighter
 				local hits = core.scene:raycast(camPos, camPos + mousePos, {
-					self.selectionHighlighter
+					self.cursorHighlighter
 				})
 
 				if #hits > 0 then
 					-- show and position the torus
-					self.selectionHighlighter.visible = true
-					self.selectionHighlighter.position = hits[1].position
-					self.selectionHighlighter.rotation = quaternion.lookRotation(hits[1].normal * 10)
-					self.selectionHighlighter.rotation = self.selectionHighlighter.rotation * quaternion.euler(math.rad(90), 0, 0)
+					self.cursorHighlighter.visible = true
+					self.cursorHighlighter.position = hits[1].position
+					self.cursorHighlighter.rotation = quaternion.lookRotation(hits[1].normal * 10) * quaternion.euler(math.rad(90), 0, 0)
 
 					-- if we've set up a wireframe in a previous frame, update its position
 					if self.hoverWireframe then
@@ -61,7 +68,7 @@ return {
 					end
 				else
 					-- hide the torus
-					self.selectionHighlighter.visible = false
+					self.cursorHighlighter.visible = false
 
 					-- remove any wireframes as the user is not hovering over anything.
 					if self.hoverWireframe then
@@ -76,8 +83,10 @@ return {
 	end,
 
 	deactivate = function(self)
-		self.selectionHighlighter:destroy()
-		self.selectionHighlighter = nil
+		core.disconnect(self.clickEvent)
+		
+		self.cursorHighlighter:destroy()
+		self.cursorHighlighter = nil
 
 		if self.hoverWireframe then
 			self.hoverWireframe:destroy()
