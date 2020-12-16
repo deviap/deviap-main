be9a37765d17cc0791c00bad834e2d869491ff61
utrain <skassem05@outlook.com>
2020-12-16 00:32:02 -0500

    Support icons and nicer buttons. Add icon variation soon?

--- source/suite/client/scripts/windows/hierarchy.lua
+++ source/suite/client/scripts/windows/hierarchy.lua
@@ -4,6 +4,8 @@
 -- Made available under the MIT License:                     --
 -- https://github.com/deviap/deviap-main/blob/master/LICENSE --
 ---------------------------------------------------------------
+local newButton = require("devgit:source/libraries/UI/components/buttons/baseButton.lua")
+
 local getDepth 
 getDepth = function(start)
 	local c = 1
@@ -30,16 +32,17 @@ construct = function(props)
 	
 	local offset = 0
 	for k,v in next, props.hierarchy do
-		core.construct("guiTextBox", {
+		local button = newButton {
 			parent = container,
 			size = guiCoord(1, 0, 0, props.buttonHeight),
 			position = guiCoord(0, 0, 0, props.buttonHeight*offset),
-			text = v.text
-		})
+			text = v.text,
+			iconId = v.icon
+		}
 
 		if v.children then
-			local childDepth = getDepth(v.children)
-			offset = offset + childDepth
+			offset = offset + 1
+			local childDepth = getDepth(v)
 			construct({
 				parent = container, 
 				hierarchy = v.children, 
@@ -47,6 +50,7 @@ construct = function(props)
 				position = guiCoord(0, 10, 0, props.buttonHeight*(offset)),
 				size = guiCoord(1, -10, 0, props.buttonHeight*(childDepth))
 			})
+			offset = offset + childDepth
 		end
 		offset = offset + 1
 	end
