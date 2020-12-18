7a1a35901947e73e66614a4f0da8497067f6ee33
utrain <skassem05@outlook.com>
2020-12-18 10:08:52 -0500

    Hierarchy mostly completed with documentation as well.

--- source/suite/client/scripts/windows/hierarchy.lua
+++ source/suite/client/scripts/windows/hierarchy.lua
@@ -4,6 +4,53 @@
 -- Made available under the MIT License:                     --
 -- https://github.com/deviap/deviap-main/blob/master/LICENSE --
 ---------------------------------------------------------------
+
+--[[
+	Name: Hierarchy Menu
+
+	Description: This is a generic hierarchy menu. Use it for your menu needs!
+	This is not standalone and it will require you to implement behaviors such
+	as click-and-drag to move elements around. But, this also means it is quite
+	generic. You also have tagging to help select only parts of the tree.
+
+	Properties:
+		parent = guiObject;
+		size = guiCoord;
+		position = guiCoord;
+		backgroundColour = colour;
+		buttonHeight = number;
+		indentBy = number;
+		children = { child, ... };
+
+		onButtonDown1 = function;
+		onButtonDown2 = function;
+		onButtonUp1 = function;
+		onButtonUp2 = function;
+		onButtonEnter = function;
+		onButtonLeave = function;
+
+	Child:
+		text = string;
+		textColour = colour;
+		font = string or enum.fonts;
+		icon = string;
+		isExpanded = boolean;
+		backgroundColour = colour;
+		backgroundAlpha = number;
+		signature = any;
+		children = { child, ... };
+
+	Interface:
+		function render(nil)
+			Rerender this component.
+		table getSubComponentFromSignature(any: id)
+			This returns the table associated with the signature
+		destroy
+			Destroy component.
+		props
+			Props being used to render component.
+--]]
+
 local newButton = require("./hierarchyButton.lua")
 
 local bindAll = function(object, events)
@@ -12,105 +59,126 @@ local bindAll = function(object, events)
 	end
 end
 
-
-local getNumOfEntries 
-getNumOfEntries = function(start)
-	local c = 0
-	if start then
-		for k,v in next, start do
-			c = c + 1 + getNumOfEntries(v.children)
-		end
+local curryFunction = function(func, arg)
+	return function(...)
+		func(arg, ...)
 	end
-	return c
 end
 
-local construct
-construct = function(props)
-	--[[
-		parent
-		size
-		position
-		backgroundColour
-		buttonHeight
-		indentBy
-		children
+local getNumberOfDescendants 
+getNumberOfDescendants = function(start)
+	local count = 0
+	if start then
+		for _, child in next, start do
+			count = count + 1 + getNumberOfDescendants(child.children)
+		end
+	end
+	return count
+end
 
-		onButtonDown1(button, signature)
-		onButtonDown2
-		onButtonUp1
-		onButtonUp2
-		onButtonEnter
-		onButtonLeave
-	]]
-
-	--[[
-		hierarchy = 
-		{
-			text
-			textColour
-			font
-			icon or image
-			isExpanded
-			backgroundColour
-			backgroundAlpha
-			signature
-			children
-		}
-	]]
-	--[[
-		render
-		getSubComponentFromSignature
-		destroy
-		props
-	]]
-
+local renderHierarchy
+renderHierarchy = function(props, __id)
 	local container = core.construct("guiFrame", {
 		parent = props.parent, 
 		size = props.size, 
 		position = props.position,
 		backgroundColour = props.backgroundColour;
 	})
-	
+
 	local offset = 0
-	for k,v in next, props.hierarchy do
-		local descendants = getNumOfEntries(v.children)
-		local button = newButton {
+
+	local renderChild
+	renderChild = function(child, inset)
+		-- Don't want to have the callback to change without calling render()
+		-- again.
+		local onButtonDown1 = props.onButtonDown1
+		local onButtonDown2 = props.onButtonDown2
+		local onButtonUp1 = props.onButtonUp1
+		local onButtonUp2 = props.onButtonUp2
+		local onButtonEnter = props.onButtonEnter
+		local onButtonExit = props.onButtonExit
+
+		local numOfDescendants = getNumberOfDescendants(child.children)
+
+		newButton {
 			parent = container,
-			size = guiCoord(1, 0, 0, props.buttonHeight),
-			position = guiCoord(0, 0, 0, props.buttonHeight*offset),
-			text = v.text,
-			iconId = v.icon,
-			fontSize = props.buttonHeight - 10,
-			backgroundColour = v.backgroundColour or props.backgroundColour;
-			textColour = v.textColour or props.textColour;
-			backgroundAlpha = v.backgroundAlpha;
-			hasDescendants = descendants > 0
+			position = guiCoord(0, props.insetBy*inset, 0, props.buttonHeight*offset),
+			size = guiCoord(1, -props.insetBy*inset, 0, props.buttonHeight),
+			backgroundColour = child.backgroundColour or props.defaultBackgroundColour;
+			backgroundAlpha = child.backgroundAlpha or props.defaultBackgroundAlpha;
+			
+			text = child.text or "",
+			textSize = props.buttonHeight - 10,
+			textColour = child.textColour or props.defaultTextColour,
+			textAlpha = child.textAlpha or props.defaultTextAlpha,
+			
+			iconId = child.iconId,
+			iconType = child.iconType or props.defaultIconType;
+			iconColour = child.iconColour or props.defaultIconColour;
+			
+			hasDescendants = numOfDescendants > 0;
+			isExpanded = child.isExpanded;
+
+			onDown1 = onButtonDown1 and function()
+				onButtonDown1(child)
+			end;
+			onDown2 = onButtonDown2 and function()
+				onButtonDown2(child)
+			end;
+			onUp1 = onButtonUp2 and function()
+				onButtonUp2(child)
+			end;
+			onUp2 = onButtonDown2 and function()
+				onButtonDown2(child)
+			end;
+			onEnter = onButtonEnter and function()
+				onButtonEnter(child)
+			end;
+			onExit = onButtonExit and function()
+				onButtonExit(child)
+			end;
 		}
+
 		offset = offset + 1
 
-		if descendants > 0 then
-			construct({
-				parent = container, 
-				hierarchy = v.children, 
-				buttonHeight = props.buttonHeight,
-				backgroundColour = props.backgroundColour,
-				textColour = props.textColour,
-				position = guiCoord(0, 10, 0, props.buttonHeight*offset),
-				size = guiCoord(1, -10, 0, props.buttonHeight*descendants)
-			})
-			offset = offset + descendants
+		if child.children and child.isExpanded then
+			for _, _child in next, child.children do
+				renderChild(_child, inset + 1)
+			end
+		end
+
+		if child.signature then
+			if __id[child.signature] then
+				error(("HIERARCHY: Two children have matching signature (%s)")
+					:format(child.signature))
+			end
+			__id[child.signature] = child
 		end
 	end
 
+	for _, child in next, props.hierarchy do
+		renderChild(child, 0)
+	end
+
+	return container
+end
+
+local construct
+construct = function(props)
+	local __id = {}
+	local container = renderHierarchy(props)
 	return {
 		render = function()
-
+			container:destroy()
+			container = renderHierarchy(props)
 		end,
 		destroy = function()
-
+			container:destroy()
+			container = nil
+			__id = nil
 		end,
 		getSubComponentFromSignature = function(signature)
-
+			return __id[signature]
 		end,
 		props = props
 	}
--- source/suite/client/scripts/windows/hierarchyTest.lua
+++ source/suite/client/scripts/windows/hierarchyTest.lua
@@ -1,5 +1,6 @@
 
-require("./hierarchy.lua"){
+local obj
+obj = require("./hierarchy.lua"){
 	parent = core.construct("guiFrame", {
 		parent = core.interface,
 		zIndex = 10,
@@ -7,88 +8,100 @@ require("./hierarchy.lua"){
 	});
 	size = guiCoord(0, 400, 0, 800);
 	position = guiCoord(0, 50, 0, 0);
-	backgroundColour = colour(0.1, 0.1, 0.1),
-	textColour = colour(0.9, 0.9, 0.9),
+	defaultBackgroundColour = colour(0.1, 0.1, 0.1),
+	defaultTextColour = colour(0.9, 0.9, 0.9),
+	onButtonEnter = function(child)
+		print("enter")
+		child.backgroundColour = colour(0.5, 0.5, 0.5)
+		obj.render()
+	end,
+	onButtonExit= function(child)
+		print("exit")
+		child.backgroundColour = colour(0.1, 0.1, 0.1)
+		obj.render()
+	end,
 	hierarchy = {
 		{
 			text = "Teams",
-			icon = "groups",
+			iconId = "groups",
+			isExpanded = true,
 			children = {
 				{
 					text = "Product Engineering",
-					icon = "engineering",
+					iconId = "engineering",
+					isExpanded = true,
 					children = {
 						{
 							text = "Jay",
-							icon = "assignment_ind"
+							iconId = "assignment_ind"
 						},
 						{
 							text = "Sanjay",
-							icon = "assignment_ind"
+							iconId = "assignment_ind"
 						},
 						{
 							text = "Ly",
-							icon = "account_circle"
+							iconId = "account_circle"
 						},
 						{
 							text = "utrain",
-							icon = "account_circle"
+							iconId = "account_circle"
 						}
 					}
 				},
 				{
 					text = "Information Experience",
-					icon = "text_snippet",
+					iconId = "text_snippet",
 					children = {
 						{
 							text = "Ryan",
-							icon = "assignment_ind",
+							iconId = "assignment_ind",
 						},
 						{
 							text = "Sanjay",
-							icon = "assignment_ind"
+							iconId = "assignment_ind"
 						},
 						{
 							text = "Neztore",
-							icon = "account_circle"
+							iconId = "account_circle"
 						},
 						{
 							text = "popeeyy",
-							icon = "account_circle"
+							iconId = "account_circle"
 						}
 					}
 				},
 				{
 					text = "Community Relations",
-					icon = "analytics",
+					iconId = "analytics",
 					children = {
 						{
 							text = "Ryan",
-							icon = "assignment_ind"
+							iconId = "assignment_ind"
 						},
 						{
 							text = "Sanjay",
-							icon = "assignment_ind",
+							iconId = "assignment_ind",
 						},
 						{
 							text = "Zaydan",
-							icon = "account_circle",
+							iconId = "account_circle",
 						},
 						{
 							text = "zachary23111",
-							icon = "account_circle",
+							iconId = "account_circle",
 						},
 						{
 							text = "Roman",
-							icon = "account_circle",
+							iconId = "account_circle",
 						},
 						{
 							text = "August",
-							icon = "account_circle"
+							iconId = "account_circle"
 						},
 						{
 							text = "Trapido02",
-							icon = "account_circle"
+							iconId = "account_circle"
 						}
 					}
 				}
@@ -96,5 +109,6 @@ require("./hierarchy.lua"){
 		},
 		
 	},
-	buttonHeight = 25
+	buttonHeight = 25,
+	insetBy = 10,
 }
