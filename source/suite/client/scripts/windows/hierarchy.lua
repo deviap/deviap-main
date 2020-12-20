34b20c094baf37ce10a6dbb2321ddc175a788f4a
utrain <skassem05@outlook.com>
2020-12-19 20:16:18 -0500

    Attempting to remove child's entry in __id breaks everything. The line was removed

--- source/suite/client/scripts/windows/hierarchy.lua
+++ source/suite/client/scripts/windows/hierarchy.lua
@@ -77,114 +77,6 @@ getNumberOfDescendants = function(start)
 	return count
 end
 
-local renderHierarchy
-renderHierarchy = function(props, __id)
-	local container = core.construct("guiFrame", {
-		parent = props.parent, 
-		size = props.size, 
-		position = props.position,
-		backgroundColour = props.defaultBackgroundColour,
-	})
-
-	local offset = 0
-
-	local renderChild
-	renderChild = function(child, inset)
-		-- Don't want to have the callback to change without calling render()
-		-- again.
-		local onButtonDown1 = props.onButtonDown1
-		local onButtonDown2 = props.onButtonDown2
-		local onButtonUp1 = props.onButtonUp1
-		local onButtonUp2 = props.onButtonUp2
-		local onButtonEnter = props.onButtonEnter
-		local onButtonExit = props.onButtonExit
-
-		local numOfDescendants = getNumberOfDescendants(child.children)
-
-		newButton {
-			parent = container,
-			position = guiCoord(0, props.insetBy * inset, 0, props.buttonHeight * offset),
-			size = guiCoord(1, -props.insetBy * inset, 0, props.buttonHeight),
-			backgroundColour = child.backgroundColour or props.defaultBackgroundColour,
-			backgroundAlpha = child.backgroundAlpha or props.defaultBackgroundAlpha,
-			
-			text = child.text or "",
-			textSize = props.buttonHeight - 10,
-			textColour = child.textColour or props.defaultTextColour,
-			textAlpha = child.textAlpha or props.defaultTextAlpha,
-			
-			iconId = child.iconId,
-			iconType = child.iconType or props.defaultIconType,
-			iconColour = child.iconColour or props.defaultIconColour,
-			
-			hasDescendants = numOfDescendants > 0,
-			isExpanded = child.isExpanded,
-
-			onDown1 = onButtonDown1 and function()
-				onButtonDown1(child)
-			end,
-			onDown2 = onButtonDown2 and function()
-				onButtonDown2(child)
-			end,
-			onUp1 = onButtonUp2 and function()
-				onButtonUp2(child)
-			end,
-			onUp2 = onButtonDown2 and function()
-				onButtonDown2(child)
-			end,
-			onEnter = onButtonEnter and function()
-				onButtonEnter(child)
-			end,
-			onExit = onButtonExit and function()
-				onButtonExit(child)
-			end,
-		}
-
-		offset = offset + 1
-
-		if child.children and child.isExpanded then
-			for _, _child in next, child.children do
-				renderChild(_child, inset + 1)
-			end
-		end
-
-		if child.signature then
-			if __id[child.signature] then
-				error(("HIERARCHY: Two children have matching signature (%s)")
-					:format(child.signature))
-			end
-			__id[child.signature] = child
-		end
-	end
-
-	for _, child in next, props.hierarchy do
-		renderChild(child, 0)
-	end
-
-	return container
-end
-
-local construct
-construct = function(props)
-	local __id = {}
-	local container = renderHierarchy(props)
-	return {
-		render = function()
-			container:destroy()
-			container = renderHierarchy(props)
-		end,
-		destroy = function()
-			container:destroy()
-			container = nil
-			__id = nil
-		end,
-		getSubComponentFromSignature = function(signature)
-			return __id[signature]
-		end,
-		props = props
-	}
-end
-
 return function(props)
 	local container = core.construct("guiFrame", { parent = props.parent })
 	local rendered = {}
@@ -210,7 +102,7 @@ return function(props)
 			local defaultIconType = props.defaultIconType or "material"
 			local defaultIconColour = props.defaultIconColour or colour(0, 0, 0)
 
-			local onButtonDown1 = props.onButton1Down
+			local onButtonDown1 = props.onButtonDown1
 			local onButtonDown2 = props.onButtonDown2
 			local onButtonUp2 = props.onButtonUp2
 			local onButtonDown2 = props.onButtonDown2
@@ -300,7 +192,6 @@ return function(props)
 				if not visited[child] then
 					button.destroy()
 					rendered[child] = nil
-					__id[child.signature] = nil
 				end
 			end
 		end
