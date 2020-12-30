338dd75eb7836b18ea367e4ca76e394ec154d345
utrain <skassem05@outlook.com>
2020-12-30 16:38:12 -0500

    Moved constructing fileExplorer into the function

--- source/suite/client/scripts/windows/fileExplorer.lua
+++ source/suite/client/scripts/windows/fileExplorer.lua
@@ -4,11 +4,12 @@
 -- Made available under the MIT License:                     --
 -- https://github.com/deviap/deviap-main/blob/master/LICENSE --
 ---------------------------------------------------------------
-local fileHierarchy = {}
-local tags = {}
 
-local parseFileToHierarchy = function(list)
-	for _, directory in next, list do
+local parseFileToHierarchy = function()
+	local fileHierarchy = {}
+	local tags = {}
+
+	for _, directory in next, core.io:list() do
 		directory = "/"..directory -- Pattern-Matching
 
 		local rootChildren = fileHierarchy
@@ -17,7 +18,9 @@ local parseFileToHierarchy = function(list)
 			if node == nil then
 				node = {
 					text = subDirectory;
-					iconId = subDirectory:match("%.") and "insert_drive_file" or "folder";
+					iconId = subDirectory:match("%.") 
+						and "insert_drive_file" 
+						or "folder";
 					children = {}
 				}
 				tags[subDirectory] = node
@@ -31,36 +34,36 @@ local parseFileToHierarchy = function(list)
 	return fileHierarchy
 end
 
-local hierarchyMenu
-hierarchyMenu = require("./hierarchy.lua"){
-	parent = core.construct("guiFrame", {
-		parent = core.interface,
-		zIndex = 10,
-		size = guiCoord(1, 0, 1, 0)
-	}),
-	size = guiCoord(0, 400, 0, 400),
-	position = guiCoord(0, 50, 0, 0),
+local fileHierarchy = parseFileToHierarchy()
 
-	defaultBackgroundColour = colour(0.0, 0.0, 0.0),
-	defaultTextColour = colour(0.9, 0.9, 0.9),
-	defaultIconColour = colour(1, 1, 1),
-	backgroundColour = colour(0, 0, 0),
-	scrollbarColour = colour(1,1,1),
+return { 
+	construct = function(parent)
+		local hierarchyMenu
+		hierarchyMenu = require("./hierarchy.lua"){
+			parent = parent,
+			size = guiCoord(0, 400, 0, 400),
+			position = guiCoord(0, 50, 0, 0),
+		
+			defaultBackgroundColour = colour(0.0, 0.0, 0.0),
+			defaultTextColour = colour(0.9, 0.9, 0.9),
+			defaultIconColour = colour(1, 1, 1),
+			backgroundColour = colour(0, 0, 0),
+			scrollbarColour = colour(1,1,1),
+		
+			buttonHeight = 25,
+			insetBy = 10,
+		
+			onButtonDown1 = function(child)
+				if child.isExpanded then
+					child.isExpanded = false
+				else
+					child.isExpanded = true
+				end
+		
+				hierarchyMenu.render()
+			end,
 
-	buttonHeight = 25,
-	insetBy = 10,
-
-	onButtonDown1 = function(child)
-		if child.isExpanded then
-			child.isExpanded = false
-		else
-			child.isExpanded = true
-		end
-
-		hierarchyMenu.render()
-	end,
-	hierarchy = parseFileToHierarchy(core.io:list())
+			hierarchy = fileHierarchy
+		}
+	end
 }
-return function()
-	parseFileToHierarchy(core.io:list())
-end
