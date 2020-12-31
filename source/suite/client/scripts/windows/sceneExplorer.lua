9afc9d2bf7386fcd0d0f6ff76b2bbb95a597b9f6
utrain <skassem05@outlook.com>
2020-12-30 22:57:52 -0500

    Minimal implementation of sceneExplorer. It does not auto-update yet.

--- source/suite/client/scripts/windows/sceneExplorer.lua
+++ source/suite/client/scripts/windows/sceneExplorer.lua
@@ -9,115 +9,77 @@ local newHierarchy = require("./hierarchy.lua")
 local parseSceneToHierarchy
 parseSceneToHierarchy = function(root)
 	local hierarchy = {}
-	hierarchy.name = root.name
-	hierarchy.icon = "stop"
+	hierarchy.text = root.name
+	hierarchy.iconId = "stop"
 	hierarchy.siginature = root
+	hierarchy.isExpanded = true
+	hierarchy.children = {}
 
-	if #root.children > 0 then
-		hierarchy.children = {}
-
-		for k,v in next, root.children do
-			hierarchy.children[k] = parseSceneToHierarchy(v)
-		end
+	for _, child in next, root.children do
+		table.insert(hierarchy.children, {
+			text = child.name,
+			iconId = "stop",
+			siginature = child,
+			hasDescendants = #child.children > 0,
+			children = {}
+		})
 	end
 
 	return hierarchy
 end
 
-return function(props)
-	local selected = {}
-
-	props.hierarchy = {}
-	props.name = "__SCENE_EXPLORER_DO_NOT_ENTER"
-	local hierarchy = newHierarchy(props)
-
-	props.onButtonDown1 = function(node, button, updateHandler)
-		if core.input:isKeyDown(enums.keys.KEY_LSHIFT) then
-			if selected[node] then
-				selected[node] = nil
-				node.backgroundColour = nil
-			else
-				selected[node] = true
-				node.backgroundColour = colour(0.8, 0.8, 0.8)
-			end
-		else
-			for k,v in next, selected do
-				k.backgroundColour = nil
-				selected[k] = nil
-			end
-
-			selected[node] = true
-			node.backgroundColour = colour(0.8, 0.8, 0.8)
-
-			if node.children and #node.children > 0 then
-				node.isExpanded = not node.isExpanded
-			end
-			hierarchy.render()
-
-		end
-
-		updateHandler()
-	end
-
-	local helper
-	helper = function(child)
-	
-		local node = {}
-		node.text = child.name
-		node.iconId = "stop"
-		node.signature = child
-		node.children = {}
-
-		if child.name == "__SCENE_EXPLORER_DO_NOT_ENTER" then
-			return node
-		end
-
-		if child.name == "outputWindow" then
-			return node
-		end
-	
-		if #child.children > 0 then
-			for k,v in next, child.children do
-				node.children[k] = helper(v)
-			end
-		end
-
-		child:on("childAdded", function(newChild)
-			local children = child.children
-			if not children[#children] then return end -- Meant object was deleted.
-			if hierarchy.getButtonFromSignature(children[#children]) then return end --Was already registered.
-			sleep(100)
-			print(child.parent.name)
-			print(child.parent.parent.name)
-			print(child.parent.parent.parent.name)
-			print(child.parent.parent.parent.parent.name)
-			node.children[#node.children+1] = helper(children[#children]) --OK ERRORS HERE
-			hierarchy.render() -- NOT FULL PICTURE
-		end)
-
-		-- child:on("childRemoved", function(newChild)
-		-- 	local newChildHierarchy = hierarchy.getButtonFromSignature(newChild)
-		-- 	for k,v in next, node.children do
-		-- 		if v == newChildHierarchy then
-		-- 			table.remove(node.children, k)
-		-- 			hierarchy.render()
-		-- 			break
-		-- 		end
-		-- 	end
-		-- end)
+return {
+	construct = function(props)
+		local hierarchyMenu
+		hierarchyMenu = require("./hierarchy.lua"){
+			parent = props.parent,
+			size = guiCoord(0, 200, 0, 400),
+			position = guiCoord(0, 40, 0, 0),
 		
-		return node
-	end
+			defaultBackgroundColour = colour.hex("FFFFFF"),
+			defaultTextColour = colour(0, 0, 0),
+			defaultIconColour = colour(0, 0, 0),
+			backgroundColour = colour.hex("FFFFFF"),
+			scrollbarColour = colour(0.5, 0.5, 0.5),
+		
+			buttonHeight = 25,
+			insetBy = 10,
+		
+			onButtonDown1 = function(node)
+				if node.isExpanded then
+					node.isExpanded = false
+					node.children = {}
+				else
+					node.isExpanded = true
+					for _, child in next, node.siginature.children do
+						table.insert(node.children, {
+							text = child.name,
+							iconId = "stop",
+							siginature = child,
+							hasDescendants = #child.children > 0,
+							children = {}
+						})
+					end
+				end
+		
+				hierarchyMenu.render()
+			end,
 
-	hierarchy.props.hierarchy = helper(core.interface).children
-	hierarchy.render()
-	return {
-		render = function()
-			hierarchy.render()
-		end;
-		destroy = function()
-			hierarchy.destroy()
-		end;
-		props = props;
-	}
-end
+			onButtonEnter = function(child, button)
+				child.backgroundColour = colour.hex("F0F0F0")
+				button.propsThenRender {
+					backgroundColour = child.backgroundColour
+				}
+			end,
+		
+			onButtonExit = function(child, button)
+				child.backgroundColour = nil
+				button.propsThenRender {
+					backgroundColour = hierarchyMenu.props.defaultBackgroundColour
+				}
+			end,
+
+			hierarchy = {parseSceneToHierarchy(core.interface)}
+		}
+	end
+}
