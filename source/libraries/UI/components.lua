da315d5c3740952205761f8d4ea64e8e4535a5ce
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2021-02-12 20:20:43 -0500

    Rebase legacy & used code.

--- source/libraries/UI/components.lua
+++ source/libraries/UI/components.lua
@@ -1,33 +1,12 @@
--- Copyright 2020 - Deviap (deviap.com)
-
+---------------------------------------------------------------
+-- Copyright 2021 Deviap (https://deviap.com/)               --
+---------------------------------------------------------------
+-- Made available under the MIT License:                     --
+-- https://github.com/deviap/deviap-main/blob/master/LICENSE --
+---------------------------------------------------------------
+-- Component quick-list.
 local components = {
-	baseComponent = require("./baseComponent.lua"),
-	
-	baseButton = require("./components/buttons/baseButton.lua"),
-	primaryButton = require("./components/buttons/primaryButton.lua"),
-	dangerButton = require("./components/buttons/dangerButton.lua"),
-	secondaryButton = require("./components/buttons/secondaryButton.lua"),
-	tertiaryButton = require("./components/buttons/tertiaryButton.lua"),
-
-	appCard = require("devgit:source/libraries/UI/components/cards/app.lua"),
-
-	dropDown = require("devgit:source/libraries/UI/components/dropDowns/dropDown.lua"),
-	dropDownOptions = require("devgit:source/libraries/UI/components/dropDowns/dropDownOptions.lua"),
-
-	textInput = require("./inputs/textInput.lua"),
-	numberInput = require("./inputs/numberInput.lua"),
-
-	inlineNotification = require("devgit:source/libraries/UI/components/notifications/inlineNotification.lua"),
-	multilineNotification = require("devgit:source/libraries/UI/components/notifications/multilineNotification.lua"),
-
-	tab = require("devgit:source/libraries/UI/components/tabs/tab.lua"),
-	tabs = require("devgit:source/libraries/UI/components/tabs/tabs.lua"),
-
-	checkbox = require("devgit:source/libraries/UI/components/checkbox.lua"),
-	loading = require("devgit:source/libraries/UI/components/loading.lua"),
-	modal = require("devgit:source/libraries/UI/components/modal.lua"),
-	toggle = require("devgit:source/libraries/UI/components/toggle.lua"),
-	tooltip = require("devgit:source/libraries/UI/components/tooltip.lua"),
+	-- RESET
 }
 
 return function(name, props)
