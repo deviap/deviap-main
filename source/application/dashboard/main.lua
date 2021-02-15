ad406afd5b2df93810b8e0783997ff7fa78d4d3c
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2021-02-14 19:12:03 -0500

    Dashboard Entry File pulls overlay on default launch.

---
+++ source/application/dashboard/main.lua
@@ -0,0 +1,9 @@
+---------------------------------------------------------------
+-- Copyright 2021 Deviap (https://deviap.com/)               --
+---------------------------------------------------------------
+-- Made available under the MIT License:                     --
+-- https://github.com/deviap/deviap-main/blob/master/LICENSE --
+---------------------------------------------------------------
+-- Main Client Application Interface File
+-- This file is required first before any other file.
+require("devgit:source/application/overlay/main.lua")
