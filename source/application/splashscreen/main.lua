5bc57b6d13793223e918caa40c39ecc2689d34e3
Jay <jay@win.local>
2021-02-16 19:28:45 +0000

    splashy wip

--- source/application/splashscreen/main.lua
+++ source/application/splashscreen/main.lua
@@ -5,97 +5,111 @@
 -- https://github.com/deviap/deviap-main/blob/master/LICENSE --
 ---------------------------------------------------------------
 -- Splashscreen Entry Point
-local button = require("devgit:source/libraries/UI/components/buttons/baseButton.lua")
-
-local devGitEnabled = core.dev.localDevGitEnabled
+local button = require(
+               				"devgit:source/libraries/UI/components/buttons/baseButton.lua")
 
 local container = core.construct("guiFrame", {
-    parent = core.engine.coreInterface,
-    position = guiCoord(0, 0, 0, 0),
-    size = guiCoord(1, 0, 1, 0),
-    backgroundColour = colour.hex("212121")
+	parent = core.engine.coreInterface,
+	position = guiCoord(0, 0, 0, 0),
+	size = guiCoord(1, 0, 1, 0),
+	backgroundColour = colour.rgb(255, 138, 101)
 })
 
 local brandLogo = core.construct("guiImage", {
-    parent = container,
-    position = guiCoord(0.5, -50, 0.5, -200), --guiCoord(0, 340, 0, 200),
-    size = guiCoord(0, 128, 0, 128),
-    image = "devgit:assets/images/logo.png",
-    backgroundAlpha = 0
+	parent = container,
+	position = guiCoord(0.5, -32, 0, -64),
+	size = guiCoord(0, 64, 0, 64),
+	image = "devgit:assets/images/logo.png",
+	backgroundAlpha = 0
 })
 
-local brandHeading = core.construct("guiTextBox", {
-    parent = container,
-    text = "Deviap",
-    position = guiCoord(0.5, -80, 0.5, 100),
-    size = guiCoord(0, 200, 0, 100),
-    backgroundAlpha = 0,
-    textSize = 55,
-    textFont = "deviap:fonts/openSansSemiBold.ttf",
-    textAlign = "middle",
-    textColour = colour.hex("FFFFFF"),
-    strokeWidth = 2,
-    strokeColour = colour.hex("F5F5F5"),
+local versionInfo = core.construct("guiTextBox", {
+	parent = brandLogo,
+	text = _TEV_VERSION,
+	position = guiCoord(1, 20, 1, -24),
+	size = guiCoord(0, 100, 0, 14),
+	backgroundAlpha = 0,
+	textAlpha = 0,
+	textSize = 14,
+	textFont = "deviap:fonts/openSansSemiBold.ttf",
+	textAlign = "middleLeft",
+	textColour = colour.hex("FFFFFF")
 })
 
-local editPackedAppButton = button {
-    parent = container,
-    position = guiCoord(1, 250, 0.5, -40),
-    size = guiCoord(0, 270, 0, 50),
-    containerBackgroundColour = colour.rgb(255, 138, 101),
-    text = "Run Packaged Application",
-    textAlign = "middle",
-    textSize = 24,
-    strokeRadius = 10,
-}
+-- Abitrary background shapes and animations
+-- Works fine without this!
+spawn(function()
+	local abstractShape1 = core.construct("guiImage", {
+		parent = container,
+		position = guiCoord(0, -1024 / 3, 0, -1024 / 2), -- guiCoord(0, 340, 0, 200),
+		size = guiCoord(0, 1024, 0, 1024),
+		image = "devgit:assets/images/abstractShape1.png",
+		backgroundAlpha = 0,
+		imageAlpha = 0.15,
+		zIndex = -2
+	})
 
-editPackedAppButton.container:on("mouseLeftUp", function()
-    -- Do something here, idk.
+	local abstractShape1Blur = core.construct("guiImage", {
+		parent = container,
+		position = guiCoord(0, 1024 / 3, 0, -1024 / 2.5), -- guiCoord(0, 340, 0, 200),
+		size = guiCoord(0, 1024 / 1.5, 0, 1024 / 1.5),
+		image = "devgit:assets/images/abstractShape1Blur.png",
+		backgroundAlpha = 0,
+		imageAlpha = 0.15,
+		rotation = math.rad(90),
+		zIndex = -2
+	})
+
+	while sleep(.1) do
+		abstractShape1.rotation = abstractShape1.rotation + math.rad(0.01)
+		abstractShape1Blur.rotation = abstractShape1Blur.rotation - math.rad(0.01)
+	end
 end)
 
-local editUnpackedAppButton = button {
-    parent = container,
-    position = guiCoord(1, 250, 0.5, 20),
-    size = guiCoord(0, 270, 0, 50),
-    containerBackgroundColour = colour.rgb(255, 138, 101),
-    text = "Run Unpackaged Application",
-    textAlign = "middle",
-    textSize = 24,
-    strokeRadius = 10,
+local buttons = {
+	{
+		text = "Docs",
+		icon = "apps",
+		clicked = function()
+
+		end
+	},
+	{
+		text = "LICENSE",
+		icon = "apps",
+		clicked = function()
+
+		end
+	}
 }
 
-editUnpackedAppButton.container:on("mouseLeftUp", function()
-    -- Do something here, idk.
-end)
+for i, v in pairs(buttons) do
+	local btn = core.construct("guiIcon", {
+		parent = container,
+		position = guiCoord(0.5, ((-#buttons*110)/2) + ((i-1) * 110), 0.5, -50),
+		size = guiCoord(0, 100, 0, 100),
+		iconMax = 24,
+		iconId = v.icon,
+		iconColour = colour.rgb(0, 0, 0),
+		backgroundAlpha = 1,
+		strokeRadius = 2
+	})
 
---[[
--- Only enabled if devgit is overriden.
-local overrideButton = button {
-    parent = container,
-    position = guiCoord(1, 250, 0.5, 20),
-    size = guiCoord(0, 180, 0, 50),
-    containerBackgroundColour = colour.rgb(229, 115, 115),
-    text = "Override",
-    textAlign = "middle",
-    textSize = 24,
-    strokeRadius = 10
-}
+    local txt = core.construct("guiTextBox", {
+        parent = btn,
+        backgroundAlpha = 0,
+        text = v.text,
+        position = guiCoord(0, 0, 0.5, 20),
+        size = guiCoord(1, 0, 0, 20),
+        textAlign = "topMiddle",
+        textSize = 14,
+        active = false
+    })
+end
 
-overrideButton.container:on("mouseLeftUp", function()
-    -- Remove Splashscreen
-    container:destroy()
-
-    -- Load in overlay because, no reason not to.
-    require("devgit:source/application/overlay/main.lua")
-end)
-]]--
-
-core.tween:begin(brandHeading, 1, {textAlpha = 0, position = guiCoord(0.5, -80, 0.5, 150)}, "outQuad")
-core.tween:begin(brandLogo, 1, {position = guiCoord(0.5, -60, 0.5, -200), size = guiCoord(0, 140, 0, 140)}, "outQuad")
-core.tween:begin(editPackedAppButton.container, 0.8, {position = guiCoord(0.5, -110, 0.5, -40)}, "inQuad")
-core.tween:begin(editUnpackedAppButton.container, 0.8, {position = guiCoord(0.5, -110, 0.5, 20)}, "inQuad")
-
--- Logic to enabled/put into view when devgit is enabled.
---[[if devGitEnabled then
-    core.tween:begin(overrideButton.container, 0.8, {position = guiCoord(0.5, -70, 0.5, 20)}, "inQuad")
-end]]--
+core.tween:begin(brandLogo, 1, {
+	position = guiCoord(0.5, -32, 0.5, -132),
+	size = guiCoord(0, 64, 0, 64)
+}, "outQuad")
+core.tween:begin(versionInfo, 1.5,
+                 {textAlpha = 0.5, position = guiCoord(1, 0, 1, -24)}, "outQuad")
